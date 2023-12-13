0b05e2d78455412689465aafc0c55d8d7741ecfc
Marcel Henning <marcel.henning@adesso.de>
2023-12-13 16:37:22 +0100

    add first components

---
+++ shoppinglist-ui/src/components/AddItemForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-form @submit.prevent="submit">
+    <v-text-field
+        v-model="newItem"
+        label="Add a new item"
+        outlined
+        dense
+    ></v-text-field>
+    <v-btn type="submit" color="primary">Add</v-btn>
+  </v-form>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const newItem = ref('');
+const emit = defineEmits(['item-added']);
+
+const submit = () => {
+  if (newItem.value.trim()) {
+    emit('item-added', newItem.value);
+    newItem.value = '';
+  }
+};
+</script>
---
+++ shoppinglist-ui/src/views/ShoppinglistView.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <add-item-form @item-added="addItem" />
+    <shopping-list :items="items" @remove-item="removeItem" />
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import ShoppingList from '@/components/Shoppinglist.vue';
+import AddItemForm from '@/components/AddItemForm.vue';
+
+const items = ref<string[]>([]);
+
+const addItem = (item: string) => {
+  items.value.push(item);
+};
+
+const removeItem = (index: number) => {
+  items.value.splice(index, 1);
+};
+</script>
