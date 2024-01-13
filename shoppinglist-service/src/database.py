b055c95ea33ca9f7052c4724e929db31872966f2
Marcel Henning <marcel.henning@adesso.de>
2024-01-13 15:26:09 +0100

    change db init

--- shoppinglist-service/src/database.py
+++ shoppinglist-service/src/database.py
@@ -15,7 +15,9 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-Base.metadata.create_all(bind=engine)
+from src.models.item import Item
+
+
 
 logger = logging.getLogger(__name__)
 
--- shoppinglist-service/src/main.py
+++ shoppinglist-service/src/main.py
@@ -3,11 +3,13 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from src.controller import item_routes
+from src.database import Base, engine
+
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://localhost:4200"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -16,6 +18,7 @@ app.add_middleware(
 
 logging.basicConfig(level=logging.INFO)
 
+Base.metadata.create_all(bind=engine)
 
 app.include_router(item_routes.router, prefix="/items", tags=["items"])
 
